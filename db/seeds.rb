37cfa6deed6ffc9f8ad5b79edd58f4d65de2eb4f
Jean-Noel Drugmand <jn.drugmand@gmail.com>
2022-08-17 10:54:43 +0200

    Exo 3 ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+articles = []
+10.times do
+  articles << Article.create([{title: Faker::TvShows::HowIMetYourMother.character, content: Faker::TvShows::HowIMetYourMother.quote}])
+end
